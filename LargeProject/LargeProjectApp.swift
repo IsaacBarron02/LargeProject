0d6fe931ec1c9eef0e6e249f2463f7868f41b993
Isaac Velazco <isaac.barron@icloud.com>
2020-10-26 17:04:55 -0600

    inital commit

---
+++ LargeProject/LargeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LargeProjectApp.swift
+//  LargeProject
+//
+//  Created by Isaac Barron Velazco on 26/10/20.
+//
+
+import SwiftUI
+
+@main
+struct LargeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
