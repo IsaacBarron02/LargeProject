885633d09dc13e21222ba53606d63ed75f67275b
Isaac Velazco <isaac.barron@icloud.com>
2020-10-26 19:58:05 -0600

    Se agrego un trivial test y se hizo la primer prueba de que se pueda actualizar el repositorio remoto

--- LargeProjectTests/LargeProjectTests.swift
+++ LargeProjectTests/LargeProjectTests.swift
@@ -21,13 +21,8 @@ class LargeProjectTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        let i=0
+        XCTAssert(i == 0, "Debería de ser 0")
     }
 
 }
